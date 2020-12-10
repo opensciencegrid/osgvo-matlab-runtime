d04438165e6a2f64cc2343b9e12d54377c7a87ba
Mats Rynge <mats@rynge.net>
2020-12-09 21:54:17 -0800

    Added R2020a

--- 90-osgvo-matlab-runtime.sh
+++ 90-osgvo-matlab-runtime.sh
@@ -1,4 +1,4 @@
 
-LD_LIBRARY_PATH=/usr/local/MATLAB/MATLAB_Runtime/v96/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v96/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v96/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v96/extern/bin/glnxa64:${LD_LIBRARY_PATH}
+LD_LIBRARY_PATH=/usr/local/MATLAB/MATLAB_Runtime/v98/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v98/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v98/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v98/extern/bin/glnxa64:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH
 
